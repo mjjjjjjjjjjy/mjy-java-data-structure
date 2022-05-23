2c5b7f7584c08c8b2118dfc79c3e12add523a6d9
磨健悦 <1194229162@qq.com>
2022-05-23 15:08:20 +0800

    树

---
+++ mjy-java-data-structure-tree/src/main/java/平衡二叉树/Application.java
@@ -0,0 +1,16 @@
+package 平衡二叉树;
+
+/**
+ * @Author: Mo Jianyue
+ * @Description
+ * @Date: 2022/5/23 下午3:06
+ * @Modified By
+ */
+public class Application {
+    public static class 平衡二叉树 {
+        public static void main(String[] args) {
+            //构建一个平衡二叉树
+
+        }
+    }
+}
